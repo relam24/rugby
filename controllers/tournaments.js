c4e8b749318c8647192ab00b37022d26e0d7e3e8
Rachel Elam <relam24@gmail.com>
2018-03-21 15:19:18 -0400

    server working perfectly

--- controllers/tournaments.js
+++ controllers/tournaments.js
@@ -2,4 +2,29 @@ const express = require('express');
 const router = express.Router();
 const Tournaments = require('../models/tournaments.js');
 
+// seed route to add into db easily
+router.get('/seed', (req, res) => {
+	res.send('seed');
+});
+
+// index
+router.get('/', (req, res) => {
+	res.render('index.ejs');
+});
+
+// new route
+router.get('/new', (req, res) => {
+	res.render('new.ejs');
+});
+
+// show route--must be under new
+router.get('/:id', (req, res) => {
+	res.render('show.ejs');
+});
+
+// edit route--can go anywhere
+router.get('/:id/edit', (req, res) => {
+	res.render('edit.ejs');
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -8,10 +8,8 @@ const methodOverride = require('method-override');
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 
-
-
-
-
+const tourController = require('./controllers/tournaments.js');
+app.use('/tournaments', tourController);
 
 mongoose.connect('mongodb://localhost:27017/rugbycrud');
 mongoose.connection.once('open', () => {
